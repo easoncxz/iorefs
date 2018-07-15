5f388782a6c2de016b4de32c11320753a8598a05
Eason Chen <easoncxz@gmail.com>
2018-07-15 15:20:39 +1200

    Make changes to queue benchmarking

--- src/QueueTesting.hs
+++ src/QueueTesting.hs
@@ -24,15 +24,22 @@ mapplyN :: Monad m => Int -> (a -> m a) -> m a -> m a
 mapplyN 0 f !a = a
 mapplyN !n f !a = mapplyN (n - 1) f (f =<< a)
 
-testQueue :: Queue q => q Int -> Int -> q Int
-testQueue q n =
+loadAndEmpty :: Queue q => q Int -> Int -> q Int
+loadAndEmpty q n =
   let fullQueue = applyN n (Queue.enqueue 1) q
       emptyQueueM = mapplyN n Queue.dequeue (return fullQueue)
    in fromMaybe Queue.empty emptyQueueM
 
+insertOne :: Queue q => q Char -> Int -> q Char
+insertOne q n =
+  let large = applyN n (Queue.enqueue '@') q
+   in Queue.enqueue '@' large
+
 benchQueue :: Queue q => String -> q Int -> Benchmark
 benchQueue name q =
-  bgroup name [bench (show n) (whnf (testQueue q) (n * 100)) | n <- [1,2 .. 10]]
+  bgroup
+    name
+    [bench (show n) (whnf (loadAndEmpty q) (n * 100)) | n <- [1,2 .. 10]]
 
 benchMain :: IO ()
 benchMain = do
