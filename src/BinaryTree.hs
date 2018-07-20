3675732ec7f231c054f65fbabbce9e572592f5df
Zong Chen <zong.chen@nec.co.nz>
2018-07-20 13:10:11 +1200

    Make `Node` type explicit

--- src/BinaryTree.hs
+++ src/BinaryTree.hs
@@ -8,34 +8,35 @@ import Test.QuickCheck (Arbitrary(arbitrary), Property, (==>), conjoin, discard)
 
 data BinaryTree a
   = EmptyTree
-  | Branch { label :: a
-           , left :: BinaryTree a
-           , right :: BinaryTree a }
+  | Branch (Node a)
   deriving (Show, Eq, Functor)
 
+data Node a = Node
+  { nLabel :: a
+  , nLeft :: BinaryTree a
+  , nRight :: BinaryTree a
+  } deriving (Show, Eq, Functor)
+
 leaf :: a -> BinaryTree a
-leaf n = Branch n EmptyTree EmptyTree
+leaf n = Branch (Node n EmptyTree EmptyTree)
 
 asLeftOf :: BinaryTree a -> a -> BinaryTree a
-asLeftOf t a = Branch a t EmptyTree
+asLeftOf t a = Branch (Node a t EmptyTree)
 
 asRightOf :: BinaryTree a -> a -> BinaryTree a
-asRightOf t a = Branch a EmptyTree t
+asRightOf t a = Branch (Node a EmptyTree t)
 
-node :: BinaryTree a -> Maybe a
-node EmptyTree = Nothing
-node (Branch n _ _) = Just n
-
-tree1 :: BinaryTree Int
-tree1 = Branch 5 (Branch 2 (leaf 1) (leaf 4)) (leaf 3)
+asBranch :: BinaryTree a -> Maybe (Node a)
+asBranch EmptyTree = Nothing
+asBranch (Branch n) = Just n
 
 inorderTraversal :: BinaryTree a -> [a]
 inorderTraversal EmptyTree = []
-inorderTraversal (Branch n l r) = inorderTraversal l ++ [n] ++ inorderTraversal r
+inorderTraversal (Branch (Node n l r)) = inorderTraversal l ++ [n] ++ inorderTraversal r
 
 preorderTraversal :: BinaryTree a -> [a]
 preorderTraversal EmptyTree = []
-preorderTraversal (Branch n l r) = [n] ++ preorderTraversal l ++ preorderTraversal r
+preorderTraversal (Branch (Node n l r)) = [n] ++ preorderTraversal l ++ preorderTraversal r
 
 -- TODO
 levelOrderTraversal :: BinaryTree a -> [a]
@@ -43,30 +44,30 @@ levelOrderTraversal _ = []
 
 head :: BinaryTree a -> Maybe a
 head EmptyTree = Nothing
-head (Branch n EmptyTree _) = Just n
-head (Branch _ l _) = head l
+head (Branch (Node n EmptyTree _)) = Just n
+head (Branch (Node _ l _)) = head l
 
 last :: BinaryTree a -> Maybe a
 last EmptyTree = Nothing
-last (Branch n _ EmptyTree) = Just n
-last (Branch _ _ r) = last r
+last (Branch (Node n _ EmptyTree)) = Just n
+last (Branch (Node _ _ r)) = last r
 
 rootPrev :: BinaryTree a -> Maybe a
 rootPrev EmptyTree = Nothing
-rootPrev (Branch _ EmptyTree _) = Nothing
-rootPrev (Branch _ l _) = last l
+rootPrev (Branch (Node _ EmptyTree _)) = Nothing
+rootPrev (Branch (Node _ l _)) = last l
 
 rootNext :: BinaryTree a -> Maybe a
 rootNext EmptyTree = Nothing
-rootNext (Branch _ _ EmptyTree) = Nothing
-rootNext (Branch _ _ r) = head r
+rootNext (Branch (Node _ _ EmptyTree)) = Nothing
+rootNext (Branch (Node _ _ r)) = head r
 
 insert :: (Ord a) => a -> BinaryTree a -> BinaryTree a
 insert a EmptyTree = leaf a
-insert a b@Branch {label, left, right} =
-  if a < label
-    then b {left = insert a left}
-    else b {right = insert a right}
+insert a (Branch (Node n l r)) =
+  if a < n
+    then Branch (Node n (insert a l) r)
+    else Branch (Node n l (insert a r))
 
 fromList :: (Ord a) => [a] -> BinaryTree a
 fromList = List.foldl' (flip insert) EmptyTree
@@ -77,64 +78,65 @@ instance (Arbitrary a, Ord a) => Arbitrary (BinaryTree a) where
 zipTree :: BinaryTree a -> BinaryTree b -> BinaryTree (a, b)
 zipTree EmptyTree _ = EmptyTree
 zipTree _ EmptyTree = EmptyTree
-zipTree (Branch a al ar) (Branch b bl br) = Branch (a, b) (zipTree al bl) (zipTree ar br)
+zipTree (Branch (Node a al ar)) (Branch (Node b bl br)) =
+  Branch (Node (a, b) (zipTree al bl) (zipTree ar br))
 
 unzipTree :: BinaryTree (a, b) -> (BinaryTree a, BinaryTree b)
 unzipTree EmptyTree = (EmptyTree, EmptyTree)
-unzipTree (Branch (a, b) l r) =
+unzipTree (Branch (Node (a, b) l r)) =
   let (la, lb) = unzipTree l
       (ra, rb) = unzipTree r
-   in (Branch a la ra, Branch b lb rb)
+   in (Branch (Node a la ra), Branch (Node b lb rb))
 
 emptyTreeHeight :: Int
 emptyTreeHeight = -1
 
 heightTree :: BinaryTree a -> BinaryTree Int
 heightTree EmptyTree = EmptyTree
-heightTree (Branch n l r) =
+heightTree (Branch (Node n l r)) =
   let lt = heightTree l
       rt = heightTree r
       maybeOneMore = do
-        tallerChild <- max (node lt) (node rt)
+        tallerChild <- max (fmap nLabel $ asBranch lt) (fmap nLabel $ asBranch rt)
         return (tallerChild + 1)
-   in Branch (fromMaybe leafNodeHeight maybeOneMore) lt rt
+   in Branch (Node (fromMaybe leafNodeHeight maybeOneMore) lt rt)
   where
     leafNodeHeight = emptyTreeHeight + 1
 
 withHeight :: BinaryTree t -> BinaryTree (t, Int)
 withHeight t = zipTree t (heightTree t)
 
-height :: BinaryTree t -> Maybe Int
-height = fmap snd . node . withHeight
+height :: BinaryTree t -> Int
+height = fromMaybe emptyTreeHeight . fmap nLabel . asBranch . heightTree
 
 readHeightLabel :: BinaryTree (a, Int) -> Maybe Int
 readHeightLabel EmptyTree = Nothing
-readHeightLabel (Branch (_, h) _ _) = Just h
+readHeightLabel (Branch (Node (_, h) _ _)) = Just h
 
 -- | Height of the tree if a tree is to be built using the two given trees as subtrees
 wouldBeHeight :: BinaryTree (a, Int) -> BinaryTree (a, Int) -> Int
 wouldBeHeight l r = 1 + fromMaybe emptyTreeHeight (max (readHeightLabel l) (readHeightLabel r))
 
-branchWithHeight :: t -> BinaryTree (t, Int) -> BinaryTree (t, Int) -> BinaryTree (t, Int)
-branchWithHeight n l r = Branch (n, wouldBeHeight l r) l r
+nodeWithHeight :: t -> BinaryTree (t, Int) -> BinaryTree (t, Int) -> Node (t, Int)
+nodeWithHeight n l r = Node (n, wouldBeHeight l r) l r
 
 insertWithHeight :: (Ord a) => a -> BinaryTree (a, Int) -> BinaryTree (a, Int)
-insertWithHeight a EmptyTree = Branch (a, emptyTreeHeight + 1) EmptyTree EmptyTree
-insertWithHeight a (Branch (n, h) l r) =
+insertWithHeight a EmptyTree = Branch (nodeWithHeight a EmptyTree EmptyTree)
+insertWithHeight a (Branch (Node (n, h) l r)) =
   if a < n
-    then branchWithHeight n (insertWithHeight a l) r
-    else branchWithHeight n l (insertWithHeight a r)
+    then Branch (nodeWithHeight n (insertWithHeight a l) r)
+    else Branch (nodeWithHeight n l (insertWithHeight a r))
 
 isSubtreeOf :: (Eq a) => BinaryTree a -> BinaryTree a -> Bool
 isSubtreeOf EmptyTree _ = True
-isSubtreeOf Branch {} EmptyTree = False
-isSubtreeOf a@(Branch an al ar) b@(Branch bn bl br) =
+isSubtreeOf (Branch _) EmptyTree = False
+isSubtreeOf a@(Branch (Node an al ar)) b@(Branch (Node bn bl br)) =
   an == bn && al `isSubtreeOf` bl && ar `isSubtreeOf` br || a `isSubtreeOf` bl || a `isSubtreeOf` br
 
 -- | Positive corresponds to a taller left-subtree
 balanceFactor :: BinaryTree (a, Int) -> Int
 balanceFactor EmptyTree = 0
-balanceFactor (Branch _ l r) =
+balanceFactor (Branch (Node _ l r)) =
   (readHeightLabel l `or` emptyTreeHeight) - (readHeightLabel r `or` emptyTreeHeight)
   where
     or mb def = fromMaybe def mb
@@ -146,27 +148,27 @@ isAVL :: (Ord a) => BinaryTree a -> Bool
 isAVL t = go (withHeight t)
   where
     go EmptyTree = True
-    go b@(Branch _ l r) = nodeAdmissable b && go l && go r
+    go b@(Branch (Node _ l r)) = nodeAdmissable b && go l && go r
 
 rotateLeft :: BinaryTree (a, Int) -> BinaryTree (a, Int)
 rotateLeft EmptyTree = EmptyTree
-rotateLeft b@(Branch _ _ EmptyTree) = b
-rotateLeft (Branch (pn, _) pl (Branch (cn, _) cl cr)) =
-  branchWithHeight cn (branchWithHeight pn pl cl) cr
+rotateLeft b@(Branch (Node _ _ EmptyTree)) = b
+rotateLeft (Branch (Node (pn, _) pl (Branch (Node (cn, _) cl cr)))) =
+  Branch (nodeWithHeight cn (Branch (nodeWithHeight pn pl cl)) cr)
 
 rotateLeftMaybe :: BinaryTree (a, Int) -> Maybe (BinaryTree (a, Int))
 rotateLeftMaybe EmptyTree = Nothing
-rotateLeftMaybe (Branch _ _ EmptyTree) = Nothing
+rotateLeftMaybe (Branch (Node _ _ EmptyTree)) = Nothing
 rotateLeftMaybe b = Just (rotateLeft b)
 
 rotateRight :: BinaryTree (a, Int) -> BinaryTree (a, Int)
 rotateRight EmptyTree = EmptyTree
-rotateRight b@(Branch _ EmptyTree EmptyTree) = b
-rotateRight b@(Branch _ EmptyTree Branch {}) = b
-rotateRight p@(Branch (pn, _) c@(Branch (cn, _) cl cr) pr) =
-  branchWithHeight cn cl (branchWithHeight pn cr pr)
+rotateRight b@(Branch (Node _ EmptyTree EmptyTree)) = b
+rotateRight b@(Branch (Node _ EmptyTree Branch {})) = b
+rotateRight p@(Branch (Node (pn, _) c@(Branch (Node (cn, _) cl cr)) pr)) =
+  Branch (nodeWithHeight cn cl (Branch (nodeWithHeight pn cr pr)))
 
 rotateRightMaybe :: BinaryTree (a, Int) -> Maybe (BinaryTree (a, Int))
 rotateRightMaybe EmptyTree = Nothing
-rotateRightMaybe (Branch _ EmptyTree _) = Nothing
+rotateRightMaybe (Branch (Node _ EmptyTree _)) = Nothing
 rotateRightMaybe b = Just (rotateRight b)
