b2ca9ea17932b1f1ee4dd31d91437f13d13c7fbc
Zong Chen <zong.chen@nec.co.nz>
2018-07-23 18:04:14 +1200

    Add a failing example of balanced deletion

--- test/Test/AVLTree.hs
+++ test/Test/AVLTree.hs
@@ -1,7 +1,7 @@
 module Test.AVLTree where
 
 import AVLTree
-import BinaryTree (BinaryTree)
+import BinaryTree (BinaryTree(Branch, Empty))
 import qualified BinaryTree as BT
 
 import Test.BinaryTree (searchProperty)
@@ -72,3 +72,173 @@ prop_deleteWithHeightAVLPreservesHeightInvariant x (AVLTree t) =
 prop_deleteWithHeightAVLPreservesAVLProperty :: Char -> AVLTree Char -> Bool
 prop_deleteWithHeightAVLPreservesAVLProperty x (AVLTree t) =
   fromMaybe True . fmap (isAVL . treeWithoutHeight) $ deleteWithHeightAVL x t
+
+badTree :: AVLTree Char
+badTree =
+  AVLTree
+    { runAVLTree =
+        Branch
+          ('R', 6)
+          (Branch
+             ('\'', 5)
+             (Branch
+                ('\n', 4)
+                (Branch
+                   ('\a', 2)
+                   (Branch
+                      ('\ENQ', 1)
+                      (Branch ('\ETX', 0) Empty Empty)
+                      (Branch ('\ENQ', 0) Empty Empty))
+                   (Branch ('\b', 0) Empty Empty))
+                (Branch
+                   ('\DC1', 3)
+                   (Branch
+                      ('\SI', 1)
+                      (Branch ('\SO', 0) Empty Empty)
+                      (Branch ('\DLE', 0) Empty Empty))
+                   (Branch
+                      ('\CAN', 2)
+                      (Branch ('\NAK', 1) (Branch ('\DC1', 0) Empty Empty) Empty)
+                      (Branch
+                         ('\US', 1)
+                         (Branch ('\EM', 0) Empty Empty)
+                         (Branch (' ', 0) Empty Empty)))))
+             (Branch
+                ('E', 4)
+                (Branch
+                   ('6', 3)
+                   (Branch
+                      (',', 2)
+                      (Branch ('*', 1) (Branch ('(', 0) Empty Empty) Empty)
+                      (Branch ('6', 0) Empty Empty))
+                   (Branch
+                      ('B', 2)
+                      (Branch ('9', 1) (Branch ('7', 0) Empty Empty) (Branch ('>', 0) Empty Empty))
+                      (Branch ('D', 0) Empty Empty)))
+                (Branch
+                   ('J', 3)
+                   (Branch
+                      ('G', 2)
+                      (Branch ('F', 1) Empty (Branch ('F', 0) Empty Empty))
+                      (Branch ('I', 1) Empty (Branch ('I', 0) Empty Empty)))
+                   (Branch ('L', 1) (Branch ('L', 0) Empty Empty) (Branch ('M', 0) Empty Empty)))))
+          (Branch
+             ('\50524', 5)
+             (Branch
+                ('o', 4)
+                (Branch
+                   ('e', 3)
+                   (Branch ('R', 1) Empty (Branch ('T', 0) Empty Empty))
+                   (Branch
+                      ('g', 2)
+                      (Branch ('g', 0) Empty Empty)
+                      (Branch ('g', 1) Empty (Branch ('h', 0) Empty Empty))))
+                (Branch
+                   ('z', 2)
+                   (Branch ('w', 1) (Branch ('p', 0) Empty Empty) (Branch ('y', 0) Empty Empty))
+                   (Branch ('\41687', 1) (Branch ('\DEL', 0) Empty Empty) Empty)))
+             (Branch
+                ('\625117', 4)
+                (Branch
+                   ('\436591', 3)
+                   (Branch
+                      ('\292061', 2)
+                      (Branch ('\266047', 1) (Branch ('\236332', 0) Empty Empty) Empty)
+                      (Branch ('\385672', 0) Empty Empty))
+                   (Branch
+                      ('\463172', 2)
+                      (Branch ('\455300', 0) Empty Empty)
+                      (Branch ('\591369', 1) (Branch ('\497715', 0) Empty Empty) Empty)))
+                (Branch
+                   ('\1079950', 3)
+                   (Branch
+                      ('\923195', 2)
+                      (Branch
+                         ('\732548', 1)
+                         (Branch ('\727662', 0) Empty Empty)
+                         (Branch ('\824778', 0) Empty Empty))
+                      (Branch ('\1068584', 0) Empty Empty))
+                   (Branch
+                      ('\1094321', 1)
+                      (Branch ('\1082926', 0) Empty Empty)
+                      (Branch ('\1111278', 0) Empty Empty)))))
+    }
+
+badTreeAfter :: BinaryTree (Char, Int)
+badTreeAfter =
+  Branch
+    ('R', 6)
+    (Branch
+       ('\'', 5)
+       (Branch
+          ('\n', 4)
+          (Branch
+             ('\a', 2)
+             (Branch ('\ENQ', 1) (Branch ('\ETX', 0) Empty Empty) (Branch ('\ENQ', 0) Empty Empty))
+             (Branch ('\b', 0) Empty Empty))
+          (Branch
+             ('\DC1', 3)
+             (Branch ('\SI', 1) (Branch ('\SO', 0) Empty Empty) (Branch ('\DLE', 0) Empty Empty))
+             (Branch
+                ('\CAN', 2)
+                (Branch ('\NAK', 1) (Branch ('\DC1', 0) Empty Empty) Empty)
+                (Branch ('\US', 1) (Branch ('\EM', 0) Empty Empty) (Branch (' ', 0) Empty Empty)))))
+       (Branch
+          ('E', 4)
+          (Branch
+             ('6', 3)
+             (Branch
+                (',', 2)
+                (Branch ('*', 1) (Branch ('(', 0) Empty Empty) Empty)
+                (Branch ('6', 0) Empty Empty))
+             (Branch
+                ('B', 2)
+                (Branch ('9', 1) (Branch ('7', 0) Empty Empty) (Branch ('>', 0) Empty Empty))
+                (Branch ('D', 0) Empty Empty)))
+          (Branch
+             ('J', 3)
+             (Branch
+                ('G', 2)
+                (Branch ('F', 1) Empty (Branch ('F', 0) Empty Empty))
+                (Branch ('I', 1) Empty (Branch ('I', 0) Empty Empty)))
+             (Branch ('L', 1) (Branch ('L', 0) Empty Empty) (Branch ('M', 0) Empty Empty)))))
+    (Branch
+       ('\50524', 5)
+       (Branch
+          ('o', 4)
+          (Branch
+             ('e', 3)
+             (Branch ('T', 0) Empty Empty)
+             (Branch
+                ('g', 2)
+                (Branch ('g', 0) Empty Empty)
+                (Branch ('g', 1) Empty (Branch ('h', 0) Empty Empty))))
+          (Branch
+             ('z', 2)
+             (Branch ('w', 1) (Branch ('p', 0) Empty Empty) (Branch ('y', 0) Empty Empty))
+             (Branch ('\41687', 1) (Branch ('\DEL', 0) Empty Empty) Empty)))
+       (Branch
+          ('\625117', 4)
+          (Branch
+             ('\436591', 3)
+             (Branch
+                ('\292061', 2)
+                (Branch ('\266047', 1) (Branch ('\236332', 0) Empty Empty) Empty)
+                (Branch ('\385672', 0) Empty Empty))
+             (Branch
+                ('\463172', 2)
+                (Branch ('\455300', 0) Empty Empty)
+                (Branch ('\591369', 1) (Branch ('\497715', 0) Empty Empty) Empty)))
+          (Branch
+             ('\1079950', 3)
+             (Branch
+                ('\923195', 2)
+                (Branch
+                   ('\732548', 1)
+                   (Branch ('\727662', 0) Empty Empty)
+                   (Branch ('\824778', 0) Empty Empty))
+                (Branch ('\1068584', 0) Empty Empty))
+             (Branch
+                ('\1094321', 1)
+                (Branch ('\1082926', 0) Empty Empty)
+                (Branch ('\1111278', 0) Empty Empty)))))
