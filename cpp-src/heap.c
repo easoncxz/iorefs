b89f5f43af7e74fb907301dd901584a6e4cb5d6b
Zong Chen <zong.chen@nec.co.nz>
2018-08-03 11:55:42 +1200

    Change base (1 -> 0) in heap representation

--- cpp-src/heap.c
+++ cpp-src/heap.c
@@ -16,11 +16,10 @@ struct MaybeInt {
 
 struct Heap initialiseHeap() {
   struct Heap h = {
-    .data = (int *) malloc(2 * sizeof(int)),
+    .data = (int *) malloc(sizeof(int)),
     .capacity = 1,
     .size = 0
   };
-  h.data[0] = 0; // never used
   return h;
 }
 
@@ -29,11 +28,23 @@ void freeHeap(struct Heap *h) {
   h->data = NULL;
 }
 
+int findParent(int pos) {
+  return (pos + 1) / 2 - 1;
+}
+
+int findLeftChild(int pos) {
+  return 2 * (pos + 1) - 1;
+}
+
+int findRightChild(int pos) {
+  return findLeftChild(pos) + 1;
+}
+
 void printHeap(struct Heap h) {
   printf("Heap(capacity=%d, size=%d): [", h.capacity, h.size);
-  for (int i = 1; i < h.size + 1; i++) {
+  for (int i = 0; i < h.size; i++) {
     printf("%d", h.data[i]);
-    if (i < h.size) {
+    if (i < h.size - 1) {
       printf(", ");
     }
   }
@@ -41,8 +52,8 @@ void printHeap(struct Heap h) {
 }
 
 void growHeap(struct Heap *h) {
-  int *bigger = (int *) malloc((1 + 2 * h->capacity) * sizeof(int));
-  for (int i = 0; i < h->size + 1; i++) {
+  int *bigger = (int *) malloc(2 * h->capacity * sizeof(int));
+  for (int i = 0; i < h->size; i++) {
     bigger[i] = h->data[i];
   }
   free(h->data);
@@ -51,8 +62,8 @@ void growHeap(struct Heap *h) {
 }
 
 void siftUpMaxHeap(struct Heap *h, int pos) {
-  while (pos > 1) {
-    int parent = pos / 2;
+  while (pos > 0) {
+    int parent = findParent(pos);
     if (h->data[pos] > h->data[parent]) {
       int temp = h->data[pos];
       h->data[pos] = h->data[parent];
@@ -63,7 +74,7 @@ void siftUpMaxHeap(struct Heap *h, int pos) {
 }
 
 void insertMaxHeap(struct Heap *h, int val) {
-  int freshIx = h->size + 1;
+  int freshIx = h->size;
   h->data[freshIx] = val;
   siftUpMaxHeap(h, freshIx);
   h->size += 1;
@@ -72,17 +83,16 @@ void insertMaxHeap(struct Heap *h, int val) {
   }
 }
 
-void siftDownMaxHeap(struct Heap *h, int pos) {
-  if (pos >= 1) {
-    int bound = h->size + 1;
-    while (pos < bound) {
-      int left = 2 * pos;
-      int right = left + 1;
+void siftDownMaxHeap(const struct Heap *h, int pos) {
+  if (pos >= 0) {
+    while (pos < h->size) {
+      int left = findLeftChild(pos);
+      int right = findRightChild(pos);
       int largestPos = pos;
-      if (right < bound && h->data[right] > h->data[largestPos]) {
+      if (right < h->size && h->data[right] > h->data[largestPos]) {
         largestPos = right;
       }
-      if (left < bound && h->data[left] > h->data[largestPos]) {
+      if (left < h->size && h->data[left] > h->data[largestPos]) {
         largestPos = left;
       }
       if (largestPos == pos) {
@@ -105,11 +115,11 @@ struct MaybeInt removeMaxMaxHeap(struct Heap *h) {
   if (h->size > 0) {
     struct MaybeInt r = {
       .isJust = 1,
-      .val = h->data[1]
+      .val = h->data[0]
     };
-    h->data[1] = h->data[h->size];
+    h->data[0] = h->data[h->size - 1];
     h->size -= 1;
-    siftDownMaxHeap(h, 1);
+    siftDownMaxHeap(h, 0);
     return r;
   } else {
     return (struct MaybeInt) {
@@ -119,19 +129,37 @@ struct MaybeInt removeMaxMaxHeap(struct Heap *h) {
   }
 }
 
+void maxHeapify(const struct Heap *h) {
+  for (int pos = h->size / 2; pos >= 0; pos--) {
+    siftDownMaxHeap(h, pos);
+  }
+}
+
 int main() {
   printf("sizeof(int): %lu\n", sizeof(int));
   printf("sizeof(int *): %lu\n", sizeof(int *));
-  struct Heap h = initialiseHeap();
 
   int xs[] = {3,3,4,1,2,7,5,6,3,7};
   int xsLength = sizeof(xs) / sizeof(int);
+
+  // struct Heap h = {
+  //   .capacity = 2 * xsLength,
+  //   .size = xsLength,
+  //   .data = (int *) malloc(2 * xsLength * sizeof(int))
+  // };
+  // for (int i = 0; i < xsLength; i++) {
+  //   h.data[i] = xs[i];
+  // }
+  // printHeap(h);
+  // maxHeapify(&h);
+  // printHeap(h);
+
+  struct Heap h = initialiseHeap();
   for (int i = 0; i < xsLength; i++) {
     insertMaxHeap(&h, xs[i]);
   }
   printHeap(h);
   printf("\n");
-
   while (h.size > 0) {
     struct MaybeInt m = removeMaxMaxHeap(&h);
     if (m.isJust) {
