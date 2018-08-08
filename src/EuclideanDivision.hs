d375de536c376b6e1e797b56ed75a76da332b48e
Zong Chen <zong.chen@nec.co.nz>
2018-08-08 15:52:53 +1200

    Fix bug in `divModEuclidean` and add a bunch of tests

--- src/EuclideanDivision.hs
+++ src/EuclideanDivision.hs
@@ -7,5 +7,28 @@ divModEuclidean a b =
           in (-q, r)
     else if a < 0
            then let (q, r) = divModEuclidean (-a) b
-                 in (-q - 1, -r + b)
+                 in if r == 0
+                      then (-q, r)
+                      else (-q - 1, -r + b)
            else quotRem a b
+
+divModEuclidean' :: Integral a => a -> a -> (a, a)
+divModEuclidean' a b =
+  if b < 0
+    then let (q, r) = divModEuclidean' a (-b)
+          in (-q, r)
+    else divMod a b
+
+divModByQuotRem :: Integral a => a -> a -> (a, a)
+divModByQuotRem a b =
+  let (q, r) = quotRem a b
+   in if r /= 0 && (a < 0 && b > 0 || a > 0 && b < 0)
+        then (q - 1, r + b)
+        else (q, r)
+
+quotRemByDivMod :: Integral a => a -> a -> (a, a)
+quotRemByDivMod a b =
+  let (q, r) = divMod a b
+   in if r /= 0 && (a < 0 && b > 0 || a > 0 && b < 0)
+        then (q + 1, r - b)
+        else (q, r)
--- test/Test/EuclideanDivision.hs
+++ test/Test/EuclideanDivision.hs
@@ -1,18 +1,20 @@
 module Test.EuclideanDivision where
 
-import EuclideanDivision (divModEuclidean)
+import EuclideanDivision
 
 import Test.QuickCheck
 
 divisionProperty :: (Int -> Int -> (Int, Int)) -> Int -> Int -> Property
 divisionProperty dm a b =
-  b /=
-  0 ==>
+  b /= 0 ==>
   let (q, r) = dm a b
    in a == q * b + r
 
 prop_divisionPropertyEuclidean :: Int -> Int -> Property
 prop_divisionPropertyEuclidean = divisionProperty divModEuclidean
+
+prop_divisionPropertyEuclidean' :: Int -> Int -> Property
+prop_divisionPropertyEuclidean' = divisionProperty divModEuclidean'
 
 prop_divisionPropertyTruncated :: Int -> Int -> Property
 prop_divisionPropertyTruncated = divisionProperty quotRem
@@ -22,21 +24,30 @@ prop_divisionPropertyFloored = divisionProperty divMod
 
 prop_nonNegativeModulus :: Int -> Int -> Property
 prop_nonNegativeModulus a b =
-  b /=
-  0 ==>
+  b /= 0 ==>
   let (_, r) = divModEuclidean a b
-   in r >= 0
+   in 0 <= r && r < abs b
+
+prop_nonNegativeModulus' :: Int -> Int -> Property
+prop_nonNegativeModulus' a b =
+  b /= 0 ==>
+  let (_, r) = divModEuclidean' a b
+   in 0 <= r && r < abs b
 
 prop_truncDivModulusSameSignAsDividend :: Int -> Int -> Property
 prop_truncDivModulusSameSignAsDividend a b =
-  b /=
-  0 ==>
+  b /= 0 ==>
   let (_, r) = quotRem a b
-   in r == 0 || compare r 0 == compare a 0
+   in r == 0 || (compare r 0 == compare a 0 && 0 < abs r && abs r < abs b)
 
 prop_floorDivModulusSameSignAsDivisor :: Int -> Int -> Property
 prop_floorDivModulusSameSignAsDivisor a b =
-  b /=
-  0 ==>
+  b /= 0 ==>
   let (_, r) = divMod a b
-   in r == 0 || compare r 0 == compare b 0
+   in r == 0 || (compare r 0 == compare b 0 && 0 < abs r && abs r < abs b)
+
+prop_divModByQuotRemIsDivMod :: Int -> Int -> Property
+prop_divModByQuotRemIsDivMod a b = b /= 0 ==> divMod a b == divModByQuotRem a b
+
+prop_quotRemByDivModIsQuotRem :: Int -> Int -> Property
+prop_quotRemByDivModIsQuotRem a b = b /= 0 ==> quotRem a b == quotRemByDivMod a b
