1d4f5c8636ea15a473379314334b2b0ea3be7da9
Zong Chen <zong.chen@nec.co.nz>
2018-08-08 15:00:20 +1200

    Add more tests on all 3 variants of divMod

--- test/Test/EuclideanDivision.hs
+++ test/Test/EuclideanDivision.hs
@@ -4,12 +4,21 @@ import EuclideanDivision (divModEuclidean)
 
 import Test.QuickCheck
 
-prop_divisionProperty :: Int -> Int -> Property
-prop_divisionProperty a b =
+divisionProperty :: (Int -> Int -> (Int, Int)) -> Int -> Int -> Property
+divisionProperty dm a b =
   b /=
   0 ==>
-  let (q, r) = divModEuclidean a b
+  let (q, r) = dm a b
    in a == q * b + r
+
+prop_divisionPropertyEuclidean :: Int -> Int -> Property
+prop_divisionPropertyEuclidean = divisionProperty divModEuclidean
+
+prop_divisionPropertyTruncated :: Int -> Int -> Property
+prop_divisionPropertyTruncated = divisionProperty quotRem
+
+prop_divisionPropertyFloored :: Int -> Int -> Property
+prop_divisionPropertyFloored = divisionProperty divMod
 
 prop_nonNegativeModulus :: Int -> Int -> Property
 prop_nonNegativeModulus a b =
@@ -17,3 +26,17 @@ prop_nonNegativeModulus a b =
   0 ==>
   let (_, r) = divModEuclidean a b
    in r >= 0
+
+prop_truncDivModulusSameSignAsDividend :: Int -> Int -> Property
+prop_truncDivModulusSameSignAsDividend a b =
+  b /=
+  0 ==>
+  let (_, r) = quotRem a b
+   in r == 0 || compare r 0 == compare a 0
+
+prop_floorDivModulusSameSignAsDivisor :: Int -> Int -> Property
+prop_floorDivModulusSameSignAsDivisor a b =
+  b /=
+  0 ==>
+  let (_, r) = divMod a b
+   in r == 0 || compare r 0 == compare b 0
