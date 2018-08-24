3878073687b930e7ab72b0e929c9ff969c3f013c
Eason Chen <easoncxz@gmail.com>
2018-08-24 17:04:58 +1200

    Maintain valid `lookup`

--- py_src/priority_dict.py
+++ py_src/priority_dict.py
@@ -58,6 +58,18 @@ class KeyPriority(Comparable):
     def _cmpkey(self):
         return self.priority
 
+def lookup_matches_array(lookup, arr):
+    try:
+        for k in lookup:
+            if arr[lookup[k]].key != k:
+                return False
+        for i, kp in enumerate(arr):
+            if lookup[kp.key] != i:
+                return False
+    except KeyError:
+        return False
+    return True
+
 class PriorityDict:
     ''' A dict ordered by ascending values
     '''
@@ -77,6 +89,10 @@ class PriorityDict:
         pd.lookup = {kp.key: i for i, kp in enumerate(self.data)}
         return pd
 
+    def _swap_with_lookup_update(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+        self.lookup[arr[i].key], self.lookup[arr[j].key] = i, j
+
     def __len__(self):
         return len(self.data)
 
@@ -89,14 +105,25 @@ class PriorityDict:
         ix = self.lookup.get(k)
         if ix is None:
             self.data.append(KeyPriority(k, p))
-            binary_heap.bubble_up(self.data, len(self.data) - 1, self.prevails)
+            self.lookup[k] = len(self.data) - 1
+            binary_heap.bubble_up(
+                    self.data,
+                    len(self.data) - 1,
+                    self.prevails,
+                    swap=self._swap_with_lookup_update)
         else:
-            old = self.data[ix]
-            self.data[ix] = p
-            if self.prevails(p, old):
-                binary_heap.bubble_up(self.data, ix, self.prevails)
+            old_kp = self.data[ix]
+            new_kp = KeyPriority(k, p)
+            self.data[ix] = new_kp
+            if self.prevails(new_kp, old_kp):
+                propagate = binary_heap.bubble_up
             else:
-                binary_heap.sink_down(self.data, ix, self.prevails)
+                propagate = binary_heap.sink_down
+            propagate(
+                self.data,
+                ix,
+                self.prevails,
+                swap=self._swap_with_lookup_update)
 
     def __getitem__(self, k):
         return self.data[self.lookup[k]].priority
@@ -105,11 +132,18 @@ class PriorityDict:
         pass    # tricky; first bubble_up then pop
 
     def __iter__(self):
+        ''' Yields keys in priority order '''
+        for k, _ in self.items():
+            yield k
+
+    def items(self):
+        ''' Yields key-priority pairs in priority order; good with `dict` '''
         # O(n) memory overhead, not smart
         # O(n * log(n)) runtime, like heapsort
         shallow = self.copy()
         while shallow:
-            yield shallow.pop().key
+            kp = shallow.pop()
+            yield kp.key, kp.priority
 
     def peek(self):
         return self.data[0]
@@ -119,27 +153,45 @@ class PriorityDict:
         if self.data:
             top = self.data[0]
             self.data[0] = leaf
-            binary_heap.sink_down(self.data, 0, self.prevails)
+            del self.lookup[top.key]
+            self.lookup[leaf.key] = 0
+            binary_heap.sink_down(
+                    self.data,
+                    0,
+                    self.prevails,
+                    swap=self._swap_with_lookup_update)
             return top
         else:
+            del self.lookup[leaf.key]
             return leaf
 
 class TestPriorityDict(unittest.TestCase):
 
-    @given(st.lists(st.integers()))
-    def test_one_by_one_inserting_and_deleting(self, xs):
+    def _one_by_one(self, xs, pd_predicate):
         pd = PriorityDict()
         for x in xs:
             pd[x] = x
-            assert binary_heap.is_valid_heap(pd.data, pd.prevails), pd
+            pd_predicate(pd)
         out = []
         while pd:
             out.append(pd.pop().key)
-            assert binary_heap.is_valid_heap(pd.data, pd.prevails), pd
-        assert out == sorted(xs), xs
+            pd_predicate(pd)
+        assert out == sorted(out), xs
 
     @given(st.lists(st.integers()))
-    def test_iteration_order(self, xs):
+    def test_one_by_one_inserting_and_deleting_maintains_heap_propperty(self, xs):
+        def has_valid_heap(pd):
+            assert binary_heap.is_valid_heap(pd.data, pd.prevails), pd
+        self._one_by_one(xs, has_valid_heap)
+
+    @given(st.lists(st.integers()))
+    def test_one_by_one_inserting_and_deleting_maintains_correct_lookup_table(self, xs):
+        def has_valid_lookup(pd):
+            assert lookup_matches_array(pd.lookup, pd.data), pd
+        self._one_by_one(xs, has_valid_lookup)
+
+    @given(st.lists(st.integers()))
+    def test_iteration_gives_unique_items_in_asc_sorted_order(self, xs):
         pd = PriorityDict({x: x for x in xs})
         assert list(pd) == sorted(set(xs)), xs
 
