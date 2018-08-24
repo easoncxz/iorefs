065ea06917aa78c2aa40a43602cd5aa3bfe5c5f1
Eason Chen <easoncxz@gmail.com>
2018-08-24 23:53:30 +1200

    Re-org and cleanup (move `Comparable` out)

---
+++ py_src/comparable_mixin.py
@@ -0,0 +1,32 @@
+
+import operator
+
+class Comparable:
+    ''' Mixin from regebro's blog from 2010.
+
+    Uses `self._cmpkey()` as comparison key.
+    '''
+
+    def _compare(self, other, op):
+        try:
+            return op(self._cmpkey(), other._cmpkey())
+        except (AttributeError, TypeError):
+            raise NotImplemented
+
+    def __eq__(self, other):
+        return self._compare(other, operator.eq)
+
+    def __ne__(self, other):
+        return self._compare(other, operator.ne)
+
+    def __lt__(self, other):
+        return self._compare(other, operator.lt)
+
+    def __le__(self, other):
+        return self._compare(other, operator.le)
+
+    def __gt__(self, other):
+        return self._compare(other, operator.gt)
+
+    def __ge__(self, other):
+        return self._compare(other, operator.ge)
--- py_src/priority_dict.py
+++ py_src/priority_dict.py
@@ -5,43 +5,11 @@ import unittest
 from hypothesis import given, assume
 import hypothesis.strategies as st
 
+from comparable_mixin import Comparable
 import binary_heap
-
-def identity(x):
-    return x
 
 def noop(*args, **kwargs):
     pass
-
-class Comparable:
-    ''' Mixin from regebro's blog from 2010.
-
-    Uses `self._cmpkey()` as comparison key.
-    '''
-
-    def _compare(self, other, op):
-        try:
-            return op(self._cmpkey(), other._cmpkey())
-        except (AttributeError, TypeError):
-            raise NotImplemented
-
-    def __eq__(self, other):
-        return self._compare(other, operator.eq)
-
-    def __ne__(self, other):
-        return self._compare(other, operator.ne)
-
-    def __lt__(self, other):
-        return self._compare(other, operator.lt)
-
-    def __le__(self, other):
-        return self._compare(other, operator.le)
-
-    def __gt__(self, other):
-        return self._compare(other, operator.gt)
-
-    def __ge__(self, other):
-        return self._compare(other, operator.ge)
 
 class KeyPriority(Comparable):
     ''' A wrapper that gets comparison from the priorities '''
