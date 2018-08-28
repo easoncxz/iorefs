cf2b0d498e9bd199141d38721e7fedd5d908eb06
Eason Chen <easoncxz@gmail.com>
2018-08-28 15:22:23 +1200

    Add some type annotations to graph_search

--- py_src/graph_search.py
+++ py_src/graph_search.py
@@ -1,75 +1,96 @@
 from pprint import pprint
 from collections import deque
+from typing import TypeVar, Iterable, Iterator, Generic, Hashable, Dict, List, Tuple, Callable, Any, Set, Type
+from typing_extensions import Protocol
+from abc import abstractmethod
+
+N = TypeVar('N', bound=Hashable)
+E = TypeVar('E')
+Unit = Tuple[()]
 
 
-class Graph:
+class Graph(Generic[N, E]):
     ''' A nested-dict matrix representation of a directed graph
 
     nodes: iterable of hashables
     edges: iterable of pairs of nodes
     '''
 
-    def __init__(self, nodes, edges):
-        self.matrix = {
-            from_node: {to_node: None
-                        for to_node in nodes}
+    def __init__(self, nodes: Iterable[N],
+                 edges: Iterable[Tuple[N, N, E]]) -> None:
+        self.matrix: Dict[N, Dict[N, E]] = {
+            from_node: dict()
             for from_node in nodes
         }
         for (f, t, dist) in edges:
             self.matrix[f][t] = dist
 
-    def out_nodes(self, from_node):
+    def out_nodes(self, from_node: N) -> Iterable[N]:
         return (
             n for n, dist in self.matrix[from_node].items()
             if dist is not None)
 
 
-sample_graph = Graph(
-    range(9), [
-        (1, 2, True),
-        (2, 3, True),
-        (3, 4, True),
-        (1, 3, True),
-        (3, 6, True),
-        (1, 5, True),
-        (5, 3, True),
-        (5, 6, True),
-        (6, 7, True),
-        (7, 3, True),
-        (5, 8, True),
-    ])
+sample_graph = Graph[int, Unit](
+    range(9),
+    [
+        (1, 2, ()),
+        (2, 3, ()),
+        (3, 4, ()),
+        (1, 3, ()),
+        (3, 6, ()),
+        (1, 5, ()),
+        (5, 3, ()),
+        (5, 6, ()),
+        (6, 7, ()),
+        (7, 3, ()),
+        (5, 8, ()),
+    ],
+)
 
+class Todo(Protocol[N]):
+    @abstractmethod
+    def __init__(self, init: Iterable[N] = None) -> None:
+        pass
 
-class TodoStack:
+    @abstractmethod
+    def add(self, elem: N) -> None:
+        pass
+
+    @abstractmethod
+    def remove(self) -> N:
+        pass
+
+class TodoStack(Generic[N]):
     def __init__(self, *args):
         self.coll = deque(*args)
 
     def __len__(self):
         return len(self.coll)
 
-    def add(self, elem):
+    def add(self, elem: N) -> None:
         self.coll.append(elem)
 
-    def remove(self):
+    def remove(self) -> N:
         return self.coll.pop()
 
 
-class TodoQueue:
+class TodoQueue(Generic[N]):
     def __init__(self, *args):
         self.coll = deque(*args)
 
     def __len__(self):
         return len(self.coll)
 
-    def add(self, elem):
+    def add(self, elem: N) -> None:
         self.coll.append(elem)
 
-    def remove(self):
+    def remove(self) -> N:
         return self.coll.popleft()
 
 
-def search(Todo, graph, start):
-    seen_nodes = set()
+def search(Todo: Type[Todo[N]], graph, start) -> Iterator[N]:
+    seen_nodes: Set[N] = set()
     todo = Todo([start])
     while todo:
         node = todo.remove()
@@ -81,7 +102,7 @@ def search(Todo, graph, start):
                 todo.add(to_node)
 
 
-def depth_first_search(graph, start):
+def depth_first_search(graph: Graph[N, E], start: N) -> Iterator[N]:
     '''
     >>> list(depth_first_search(sample_graph, 1))
     [1, 5, 8, 6, 7, 3, 4, 2]
